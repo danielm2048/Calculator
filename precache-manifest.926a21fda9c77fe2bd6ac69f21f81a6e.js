645cb19e712655c338399af9995f38100d25aa72
danielm2048 <danielmimoun98@gmail.com>
2019-12-19 00:46:33 +0200

    Updates

---
+++ precache-manifest.926a21fda9c77fe2bd6ac69f21f81a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439f41faf7cf40e06400be0077b61587",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "fb03b30bfbb913e2fe8a",
+    "url": "/Calculator/static/css/main.6c46d17d.chunk.css"
+  },
+  {
+    "revision": "eb8e6ab5eb550cf60c1e",
+    "url": "/Calculator/static/js/2.31ec811e.chunk.js"
+  },
+  {
+    "revision": "fb03b30bfbb913e2fe8a",
+    "url": "/Calculator/static/js/main.3dd3046a.chunk.js"
+  },
+  {
+    "revision": "0abc952091842eec6a22",
+    "url": "/Calculator/static/js/runtime-main.e1673113.js"
+  }
+]);
